67d386d704708b4036e896495594d23901e943fa
wasim-eb <Wasse.elb@gmail.com>
2022-03-28 20:16:49 +0000

    Remove comment between if/else sentence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // A list of questions and their answers. Only one answer per question is correct.
-const quizData = [
-    {
+const quizData = [{
         question: "Who played Marcus Aurelius?",
         a: "James Franco",
         b: "Joaquin Phoenix",
@@ -35,8 +34,8 @@ const quizData = [
         c: "Illidan",
         correct: "a",
     },
-  ];
-  
+];
+
 // Creates variables for the various html elements
 const quiz = document.getElementById('quiz');
 const answerElements = document.querySelectorAll('.answer');
@@ -46,13 +45,13 @@ const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const submit = document.getElementById('submit');
 const playgame = document.getElementById('playgame');
-submit.style.visibility='hidden';
-c_text.style.visibility='hidden';
-b_text.style.visibility='hidden';
-a_text.style.visibility='hidden';
+submit.style.visibility = 'hidden';
+c_text.style.visibility = 'hidden';
+b_text.style.visibility = 'hidden';
+a_text.style.visibility = 'hidden';
 questionElement.innerText = 'Welcome to the Gladiator Quizgame\nHere you will answer 5 questions about the Gladiator movie';
 answerElements.forEach(answerEl => {
-    answerEl.style.visibility='hidden';
+    answerEl.style.visibility = 'hidden';
 });
 // document.getElementById('a').style.visibility='hidden';
 // document.getElementById('b').style.visibility='hidden';
@@ -60,36 +59,36 @@ answerElements.forEach(answerEl => {
 
 
 playgame.addEventListener('click', () => {
-    playgame.style.visibility='hidden'
+    playgame.style.visibility = 'hidden'
     showElements();
     loadQuestion();
 });
 
 
 
-  
+
 let currentQuestion = 0;
 let score = 0;
 
-function showElements(){
-    submit.style.visibility='visible';
-    c_text.style.visibility='visible';
-    b_text.style.visibility='visible';
-    a_text.style.visibility='visible';
-    questionElement.style.visibility='visible';
+function showElements() {
+    submit.style.visibility = 'visible';
+    c_text.style.visibility = 'visible';
+    b_text.style.visibility = 'visible';
+    a_text.style.visibility = 'visible';
+    questionElement.style.visibility = 'visible';
     answerElements.forEach(answerEl => {
-        answerEl.style.visibility='visible';
+        answerEl.style.visibility = 'visible';
     });
 }
 // Starts the game
 // loadQuestion();
 
 // Sets up a new question for the quiz
-function loadQuestion(){
+function loadQuestion() {
     deselectAnswers();
-  
+
     const currentQuizData = quizData[currentQuestion];
-    
+
     // Sets up the current question and answers
     questionElement.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
@@ -98,46 +97,44 @@ function loadQuestion(){
 }
 
 // Deselects all the radio buttons
-function deselectAnswers(){
+function deselectAnswers() {
     answerElements.forEach(answerEl => answerEl.checked = false)
 }
 // Gets the id of the selected radio button
-function getSelected(){
+function getSelected() {
     let answer;
     // Loops through every radio button to find the selected one
     answerElements.forEach(answerEl => {
-        if(answerEl.checked){
+        if (answerEl.checked) {
             answer = answerEl.id;
         }
     });
     // Returns the id number for the selected radio button
     return answer;
-  }
-  
+}
+
 // Defines the logic for the submit button (on-click)
 submit.addEventListener('click', () => {
     // Gets the id number for the selected radio button
     const answer = getSelected();
-    
-    if(answer){
+
+    if (answer) {
         // Compares the user's selected answer to the correct answer
-        if(answer === quizData[currentQuestion].correct){
+        if (answer === quizData[currentQuestion].correct) {
             // Adds one point to the score
             score++;
         }
         // Advances the question selector 
         currentQuestion++;
-  
-        // Checks if the end of the quiz has been reached
-        if(currentQuestion < quizData.length){
+
+        // Checks if the end of the quiz has been reached, otherwise
+        // show feedback to the player
+        if (currentQuestion < quizData.length) {
             loadQuestion();
-        }
-        // Gives feedback to the player
-        else{
+        } else {
             quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions</h2>
             <button onclick="location.reload()">Start again</button>
             `;
         }
     }
-  });
-  
+});
