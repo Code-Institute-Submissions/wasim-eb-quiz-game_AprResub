2beebdeb9b2f78be5904984f7c01de6814780b63
wasim-eb <Wasse.elb@gmail.com>
2022-03-28 21:21:52 +0000

    Refactor code and add stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,51 +39,80 @@ const quizData = [{
 // Creates variables for the various html elements
 const quiz = document.getElementById('quiz');
 const answerElements = document.querySelectorAll('.answer');
+const answerLabelElements = document.querySelectorAll('.answer-label');
 const questionElement = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
 const submit = document.getElementById('submit');
 const playgame = document.getElementById('playgame');
-submit.style.visibility = 'hidden';
-c_text.style.visibility = 'hidden';
-b_text.style.visibility = 'hidden';
-a_text.style.visibility = 'hidden';
-questionElement.innerText = 'Welcome to the Gladiator Quizgame\nHere you will answer 5 questions about the Gladiator movie';
-answerElements.forEach(answerEl => {
-    answerEl.style.visibility = 'hidden';
-});
-// document.getElementById('a').style.visibility='hidden';
-// document.getElementById('b').style.visibility='hidden';
-// document.getElementById('c').style.visibility='hidden';
-
-
-playgame.addEventListener('click', () => {
-    playgame.style.visibility = 'hidden'
-    showElements();
-    loadQuestion();
-});
-
-
-
-
+const stats = document.getElementById('stats');
 let currentQuestion = 0;
+
 let score = 0;
 
+/**
+ * 
+ */
+function showAnswers(show) {
+    answerElements.forEach((el) => {
+        el.style.visibility = show ? 'visible' : 'hidden';
+    });
+
+    answerLabelElements.forEach((el) => {
+        el.style.visibility = show ? 'visible' : 'hidden';
+    });
+}
+
+/**
+ * 
+ */
+function addEventHandlers() {
+    playgame.addEventListener('click', () => {
+        playgame.style.visibility = 'hidden'
+        showElements();
+        loadQuestion();
+    });
+
+    // Defines the logic for the submit button (on-click)
+    submit.addEventListener('click', () => {
+        // Gets the id number for the selected radio button
+        const answer = getSelected();
+
+        if (answer) {
+            // Compares the user's selected answer to the correct answer
+            if (answer === quizData[currentQuestion].correct) {
+                // Adds one point to the score
+                score++;
+            }
+            // Advances the question selector 
+            currentQuestion++;
+
+            // Checks if the end of the quiz has been reached, otherwise
+            // show feedback to the player
+            if (currentQuestion < quizData.length) {
+                loadQuestion();
+            } else {
+                quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions</h2>
+                <button onclick="location.reload()">Start again</button>
+                `;
+            }
+        }
+    });
+}
+
+/**
+ * 
+ */
 function showElements() {
     submit.style.visibility = 'visible';
-    c_text.style.visibility = 'visible';
-    b_text.style.visibility = 'visible';
-    a_text.style.visibility = 'visible';
     questionElement.style.visibility = 'visible';
     answerElements.forEach(answerEl => {
         answerEl.style.visibility = 'visible';
     });
+    showAnswers(true);
 }
-// Starts the game
-// loadQuestion();
 
-// Sets up a new question for the quiz
+/**
+ * Sets up a new question for the quiz
+ */
 function loadQuestion() {
     deselectAnswers();
 
@@ -91,16 +120,25 @@ function loadQuestion() {
 
     // Sets up the current question and answers
     questionElement.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
+
+    const answers = [currentQuizData.a, currentQuizData.b, currentQuizData.c];
+    answerLabelElements.forEach((el, index) => {
+        el.innerText = answers[index];
+    });
+
+    stats.innerText = currentQuestion + "/" + quizData.length;
 }
 
-// Deselects all the radio buttons
+/**
+ * Deselects all the radio buttons
+ */
 function deselectAnswers() {
     answerElements.forEach(answerEl => answerEl.checked = false)
 }
-// Gets the id of the selected radio button
+
+/**
+ * Gets the id of the selected radio button
+ */
 function getSelected() {
     let answer;
     // Loops through every radio button to find the selected one
@@ -113,28 +151,14 @@ function getSelected() {
     return answer;
 }
 
-// Defines the logic for the submit button (on-click)
-submit.addEventListener('click', () => {
-    // Gets the id number for the selected radio button
-    const answer = getSelected();
+/**
+ * 
+ */
+function startGame() {
+    submit.style.visibility = 'hidden';
+    showAnswers(false);
+    questionElement.innerText = 'Welcome to the Gladiator Quizgame\nHere you will answer 5 questions about the Gladiator movie';
+    addEventHandlers();
+}
 
-    if (answer) {
-        // Compares the user's selected answer to the correct answer
-        if (answer === quizData[currentQuestion].correct) {
-            // Adds one point to the score
-            score++;
-        }
-        // Advances the question selector 
-        currentQuestion++;
-
-        // Checks if the end of the quiz has been reached, otherwise
-        // show feedback to the player
-        if (currentQuestion < quizData.length) {
-            loadQuestion();
-        } else {
-            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions</h2>
-            <button onclick="location.reload()">Start again</button>
-            `;
-        }
-    }
-});
+startGame();
